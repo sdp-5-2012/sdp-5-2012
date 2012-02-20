c1dd0ef2ddc6f1b090fadd0b5e1ac68ac095726b
Sam Rose <srose34@gmail.com>
2012-02-20 19:15:36 +0000

    Replaced findMode with removeVal

--- src/Planning/Runner.java
+++ src/Planning/Runner.java
@@ -10,7 +10,7 @@ import Planning.Move;
 import JavaVision.*;
 
 public class Runner extends Thread {
- 
+
 	// Objects
 	public static Ball ball;
 	public static Robot nxt;
@@ -29,7 +29,7 @@ public class Runner extends Thread {
 	public static final int EACH_WHEEL_SPEED = 900; // used for each_wheel_speed method in Robot
 
 	public static void main(String args[]) {
-		
+
 		instance = new Runner();
 
 	}
@@ -38,7 +38,7 @@ public class Runner extends Thread {
 	 * Instantiate objects and start the planning thread
 	 */
 	public Runner() {
-		
+
 		blueRobot = new Robot();
 		yellowRobot = new Robot();
 		ball = new Ball();
@@ -114,34 +114,34 @@ public class Runner extends Thread {
 
 		for(int i = 1; i <= 20; i++) {
 			getPitchInfo();
-			
+
 			try {
 				Thread.sleep(50);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 			int m = move.getAngleToBall(nxt, ball);
 			System.out.println("m: " + m);
-			
+
 			if (i < 11) {
 				prevResults[i-1] = m;
 				System.out.println("VALUE IN PREVRESULT: " + prevResults[i-1]);
 			}
 		}
-		
-		mode = findMode(prevResults);
+
+		mode = removeVal(prevResults);
 
 		for (int j = 0; j < mode.size(); j++) {
 			angle += mode.get(j);
 		}
 
 		angle /= mode.size();
-		
+
 		System.out.println("First angle(avg) calculated: " + (angle));
-		
+
 		int dist = move.getDist(nxt, ball);
-	
+
 		nxt.rotateRobot(angle);
 		try {
 			Thread.sleep(3000);
@@ -151,27 +151,27 @@ public class Runner extends Thread {
 		}
 		nxt.moveForward(20);
 		int counter = 0;
-		
+
 		while(dist > 30) { // dist in pixels
-			
-		//	System.out.println("COUNTER IS: " + counter);
-//			try {
+
+			//	System.out.println("COUNTER IS: " + counter);
+			//			try {
 			//				Thread.sleep(1000);
 			//			} catch (InterruptedException e) {
 			//				// TODO Auto-generated catch block
 			//				e.printStackTrace();
 			//			}
 			if  (counter==15) {
-				
+
 				counter=0;
-				
+
 				getPitchInfo();
 				dist = move.getDist(nxt, ball);
 				int n = move.getAngleToBall(nxt, ball);
-				
+
 				System.out.println("THIS IS THE DISTANCE: " +dist);
 				System.out.println("THIS IS THE ANGLE: " + n);
-				
+
 				if((Math.abs(n) > 20)) {
 					System.out.println("MAINLOOP: WITHIN IF STATEMENT: ANGLE TO BALL: " + n + " " + dist);
 					nxt.rotateRobot(n);
@@ -184,46 +184,79 @@ public class Runner extends Thread {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-					
+
 					System.out.println("END OF IF ANGLE: " + n2);
 					nxt.moveForward(20);
-					
+
 				}
 			}
 			counter++;
 		}
-		
+
 		nxt.stop();	
 	}
 
 
-	private ArrayList<Integer> findMode(int[] nums) {
+	/*
+	 * Compare the first element with all other elements.
+	 * If difference > 40 then place in 2nd array
+	 * Compare lengths of arrays, bigger array has more similar elements
+	 * Repeat the comparison step on the bigger array
+	 * to make sure that the array only contains similar elements
+	 */
+
+	private ArrayList<Integer> removeVal(int[] nums) {
 
 		ArrayList<Integer> array1 = new ArrayList<Integer>();
 		ArrayList<Integer> array2 = new ArrayList<Integer>();
-
+		ArrayList<Integer> array3 = new ArrayList<Integer>(); 
 
 		array1.add(nums[0]);
+		/*
+		 * if i-th element in the array differs by more than 40 from the first
+		 * element, add to 2nd array instead of 1st array
+		 */
 
-		for (int i = 1; i < nums.length; i++) {
-			if (nums[i] !=0){
-				if (Math.abs(array1.get(0) - nums[i]) <= 20) {
-					array1.add(nums[i]);
-					System.out.println("array1: " + nums[i]);
-				} else {
-					array2.add(nums[i]);
-					System.out.println("array2: " + nums[i]);
-				}
+		for (int i = 1; i < nums.length; i++ ) {
+			if ((nums[i]-nums[0] >= -20) && (nums[i]-nums[0] <= 20)) {
+				array1.add(nums[i]);
+			} else {
+				array2.add(nums[i]);
 			}
 		}
 
+		/*
+		 * Check which is bigger, set bigger one to array1
+		 */
 
 		if (array2.size() > array1.size()) {
-			return array2;
-		} else {
-			return array1;
+			array1 = (ArrayList<Integer>) array2.clone();
 		}
 
+		array2.clear();
+		array2.add(array1.get(0));
+
+		/*
+		 * same as previous for loop, but compare elements in the newly made array
+		 */
+
+		for (int i = 1; i < array1.size(); i++) {
+			if ((array1.get(i)-array1.get(0) >= -20) && (array1.get(i)-array1.get(0) <= 20)) {
+				array2.add(array1.get(i));
+			} else {
+				array3.add(array1.get(i));
+			}
+		}
+
+		/*
+		 * return larger array
+		 */
+
+		if (array3.size() > array2.size()) {
+			return array3;
+		} else {
+			return array2;
+		}
 	}
 
 	/**
@@ -236,7 +269,7 @@ public class Runner extends Thread {
 		System.out.println("______________new pitch info_______________________");
 
 		ball.setCoors(new Position(state.getBallX(), state.getBallY()));	
-		
+
 		if(teamYellow) {
 			nxt.setAngle(state.getYellowOrientation());
 			nxt.setCoors(new Position(state.getYellowX(), state.getYellowY()));
